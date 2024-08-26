ed151ae64522cd06ff4d3ddfe87ac968f0a3924a
João Pedro Borges <61915220+JPCBRGS@users.noreply.github.com>
2024-08-26 15:50:31 -0300

    Rotas iniciais, modularização

--- album-rating-platform-backend/src/index.js
+++ album-rating-platform-backend/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { connectToDatabase } = require('./config/database');
-const userRouter = require('./routes/userRoutes');
+const router = require('./routes/routes');
 
 const app = express();
 const port = 3000;
@@ -9,7 +9,7 @@ app.use(express.json());
 
 connectToDatabase();
 
-app.use('/users', userRouter);
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
